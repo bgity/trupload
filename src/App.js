093032dbb3f2ac11eaa21fc0ca71fa44783116f4
bpgity <bhagwanpawar07@gmail.com>
2020-12-02 10:15:11 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import '@aws-amplify/ui/dist/style.css';
 import DataForm from './component/uploaddata/DataForm';
 import Dashboard from './component/dashboard/Dashboard';
 import vodupload from './component/dashboardtiles/vodupload/VodUpload';
+import LiveEventUpload from './component/dashboardtiles/liveeventupload/LiveEventUpload';
+import MyAssets from './component/dashboardtiles/myassets/MyAssets';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -14,6 +16,8 @@ class App extends Component {
         <Switch>
           <Route path='/' exact component={Dashboard} />
           <Route path='/vodupload' component={vodupload} />
+          <Route path='/myassets' component={MyAssets} />
+          <Route path='/liveeventupload' component={LiveEventUpload} />
         </Switch>
       </BrowserRouter>
     );
--- src/component/dashboard/Dashboard.jsx
+++ src/component/dashboard/Dashboard.jsx
@@ -3,29 +3,35 @@ import { Col, Row, Container, Card, Button } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
 class Dashboard extends Component {
-  clickHandler = () => {
-    this.props.history.push('/vodupload');
+  clickHandler = (tiles) => {
+    if (tiles === 'myassets') {
+      this.props.history.push('/myassets');
+    } else if (tiles === 'vodupload') {
+      this.props.history.push('/vodupload');
+    } else {
+      this.props.history.push('/liveeventupload');
+    }
   };
   render() {
     return (
       <div className='top-padding'>
         <Container>
           <Row>
-            <Col sm={4} onClick={this.clickHandler}>
+            <Col sm={4} onClick={() => this.clickHandler('myassets')}>
               <Card className='card-gardient'>
                 <Card.Body>
                   <p className='text-white text-middle'>MY ASSETS</p>
                 </Card.Body>
               </Card>
             </Col>
-            <Col sm={4} onClick={this.clickHandler}>
+            <Col sm={4} onClick={() => this.clickHandler('vodupload')}>
               <Card className='card-gardient'>
                 <Card.Body>
                   <p className='text-white text-middle'>VOD UPLOAD</p>
                 </Card.Body>
               </Card>
             </Col>
-            <Col sm={4} onClick={this.clickHandler}>
+            <Col sm={4} onClick={() => this.clickHandler('liveeventupload')}>
               <Card className='card-gardient'>
                 <Card.Body>
                   <p className='text-white text-middle'> LIVE EVENT UPLOAD</p>
